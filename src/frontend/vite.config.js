20b2d11574f329f16a6643151aa3e19024ac7623
CasperHK <liwaikin2@gmail.com>
2025-09-26 01:03:40 +0800

    fix: Update tsconfig and vite config to ensure correct path aliases for components and layouts

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@dialogs': path.resolve(__dirname, 'src/components/dialogs'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
     },
   },
 });
